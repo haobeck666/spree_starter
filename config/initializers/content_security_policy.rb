1596a8874a0bf1c8cea81c7da071c39b2ed246ad
haobeck666 <zhenghao@javastore.cn>
2025-05-28 18:49:53 +0800

    Update content_security_policy.rb

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -24,8 +24,5 @@
 #   # config.content_security_policy_report_only = true
 # end
 Rails.application.config.content_security_policy do |policy|
-  policy.default_src :self
-  policy.style_src :self, :https, -> { "'nonce-#{request.content_security_policy_nonce}'" }
-  policy.script_src :self, :https
-  policy.font_src    :self, :https, :data
+  policy.style_src :self, :https, :unsafe_inline # 测试用：可替换为 nonce 后再优化
 end
