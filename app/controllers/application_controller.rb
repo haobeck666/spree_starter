20f80feabfdeccf5520e6fac9035cbc71ee09c85
haobeck666 <zhenghao@javastore.cn>
2025-05-28 18:34:46 +0800

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_csp_nonce
+
+  private
+
+  def set_csp_nonce
+    request.content_security_policy_nonce_generator = -> { SecureRandom.base64(16) }
+  end
 end
